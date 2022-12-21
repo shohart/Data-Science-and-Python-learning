6cf824b2fd798cf362c4a9e29cff2830705ec34d
Shohart <s.sovetnik@gmail.com>
2022-12-21 12:07:09 +0200

    practice

--- skillfactory plus/python_14 data praparation/ds_functions.py
+++ skillfactory plus/python_14 data praparation/ds_functions.py
@@ -17,3 +17,16 @@ def outliers_iqr_mod(data, feature, left=1.5, right=1.5):
     outliers = data[(x<lower_bound) | (x > upper_bound)]
     cleaned = data[(x>lower_bound) & (x < upper_bound)]
     return outliers, cleaned
+
+def outliers_z_score(data, feature, log_scale=False):
+    if log_scale:
+        x = np.log(data[feature]+1)
+    else:
+        x = data[feature]
+    mu = x.mean()
+    sigma = x.std()
+    lower_bound = mu - 3 * sigma
+    upper_bound = mu + 3 * sigma
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
