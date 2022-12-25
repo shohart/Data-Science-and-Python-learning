f99c6e321a8bfeafb592766ebb320ba9a759a9d0
Shohart <s.sovetnik@gmail.com>
2022-12-25 10:49:10 +0200

    minor cleaning

--- skillfactory plus/python_14 data praparation/self_task_TSUM/ds_functions.py
+++ skillfactory plus/python_14 data praparation/self_task_TSUM/ds_functions.py
@@ -1,15 +1,5 @@
 import numpy as np
 
-def outliers_iqr(data, feature):
-    x = data[feature]
-    quartile_1, quartile_3 = x.quantile(0.25), x.quantile(0.75),
-    iqr = quartile_3 - quartile_1
-    lower_bound = quartile_1 - (iqr * 1.5)
-    upper_bound = quartile_3 + (iqr * 1.5)
-    outliers = data[(x<lower_bound) | (x > upper_bound)]
-    cleaned = data[(x>lower_bound) & (x < upper_bound)]
-    return outliers, cleaned
-
 def outliers_iqr_mod(data, feature, left=1.5, right=1.5, log_scale=False):
     if log_scale:
         x = np.log(data[feature])
@@ -21,19 +11,6 @@ def outliers_iqr_mod(data, feature, left=1.5, right=1.5, log_scale=False):
     upper_bound = quartile_3 + (iqr * right)
     outliers = data[(x<lower_bound) | (x > upper_bound)]
     cleaned = data[(x>lower_bound) & (x < upper_bound)]
-    return outliers, cleaned
-
-def outliers_z_score(data, feature, log_scale=False):
-    if log_scale:
-        x = np.log(data[feature]+1)
-    else:
-        x = data[feature]
-    mu = x.mean()
-    sigma = x.std()
-    lower_bound = mu - 3 * sigma
-    upper_bound = mu + 3 * sigma
-    outliers = data[(x < lower_bound) | (x > upper_bound)]
-    cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
 
 def outliers_z_score_mod(data, feature, log_scale=False, left=3, right=3):
